c3a35e2d50721b22541c8beac72163ea7009215a
Brook Rodia <brookrodia@gmail.com>
2021-11-28 18:35:07 -0500

    Update scene selection logic

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ from console_writer import ConsoleWriter
 from scene_controller import SceneController
 from scenes import one_a, three_a, three_b, two_a, two_b
 
+# Instantiate Class Objects
+console = ConsoleWriter()
+scene = SceneController()
 
 if __name__ == "__main__":
-    # Instantiate Class Objects
-    console = ConsoleWriter()
-    scene = SceneController()
 
     # Print credits and game menu
     print(menu.title_banner)
     playsound("audio/theme.wav", False)
-    sleep(.5)
+    sleep(0.5)
     print(menu.credits_1)
-    sleep(.5)
+    sleep(0.5)
     print(menu.credits_2)
     print("")
     print("")
@@ -28,8 +28,7 @@ if __name__ == "__main__":
     print("")
     print("")
 
-    # When user enters 's' start the game
-    if scene.validate_user_input("s", menu.instructions):
-        # console.write(one_a.msg)
-        console.write(one_a.img, effect=False)
-        scene.que_next_scene(one_a, two_a, two_b)
+    # console.write(f"{menu.instructions} \n", effect=False)
+    scene.start(one_a, two_a, two_b)
+    scene.start(two_a, three_a, three_b)
+
